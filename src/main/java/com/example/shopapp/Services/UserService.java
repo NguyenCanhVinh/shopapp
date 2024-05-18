68dfda303ce67b64fdb0ba1f8a9bc244c6cc7949
Nguyen Canh Vinh <vinh.nguyencanh10@mobifone.vn>
2024-05-18 15:43:51 +0700

    code user

--- src/main/java/com/example/shopapp/Services/UserService.java
+++ src/main/java/com/example/shopapp/Services/UserService.java
@@ -1,4 +1,11 @@
 package com.example.shopapp.Services;
 
+import com.example.shopapp.dto.UserDTO;
+import com.example.shopapp.models.User;
+
 public interface UserService {
+
+  User createUser(UserDTO userDTO);
+
+  String login(String phoneNumber, String password);
 }
