bcd1f67a933c33c3d455fa8d227dda30496b9afc
Nguyen Canh Vinh <vinh.nguyencanh10@mobifone.vn>
2024-05-20 13:10:01 +0700

    creater code

--- src/main/java/com/example/shopapp/Services/UserServiceImpl.java
+++ src/main/java/com/example/shopapp/Services/UserServiceImpl.java
@@ -1,14 +1,47 @@
 package com.example.shopapp.Services;
 
+import com.example.shopapp.dto.UserDTO;
+import com.example.shopapp.models.Role;
+import com.example.shopapp.models.User;
+import com.example.shopapp.repositories.RoleRepository;
 import com.example.shopapp.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import javax.xml.datatype.DatatypeConfigurationException;
+
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService{
 
   private final UserRepository userRepository;
+  private final RoleRepository roleRepository;
 
-  public UserServiceImpl( UserRepository userRepository) {
-    this.userRepository = userRepository;
+  @Override
+  public User createUser(UserDTO userDTO) {
+    String phoneNumber= userDTO.getPhoneNumber();
+    if (userRepository.existsByPhoneNumber(phoneNumber)){
+      throw  new DataIntegrityViolationException("phone number khong ton tai!");
+    }
+    //conver từ userDto sang user
+    User newUser= User.builder()
+      .fullName(userDTO.getFullName())
+      .phoneNumber(userDTO.getPhoneNumber())
+      .password(userDTO.getPassword())
+      .address(userDTO.getAddress())
+      .dateOfBirth(userDTO.getDateBirth())
+      .facebookAccountId(userDTO.getFacebookAccountId())
+      .googleAccountId(userDTO.getGoogleAccountId())
+      .build();
+    Role role= roleRepository.findById(userDTO.getRoleId())
+      .orElseThrow(() ->)
+    return null;
+  }
+
+  @Override
+  public String login(String phoneNumber, String password) {
+    return null;
   }
 }
+
--- src/main/java/com/example/shopapp/repositories/UserRepository.java
+++ src/main/java/com/example/shopapp/repositories/UserRepository.java
@@ -4,6 +4,11 @@ import com.example.shopapp.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+  boolean existsByPhoneNumber(String phoneNumber);
+
+  Optional<User> findByPhoneNumber(String phoneNumber);
 }
